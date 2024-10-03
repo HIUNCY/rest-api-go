fd16f846b25f8375570c6b0fe3789f399c7ba055
Muhamad Zainul Kamal <zainulkamal393@gmail.com>
2024-10-03 15:08:11 +0700

    form binding

--- model/user_model.go
+++ model/user_model.go
@@ -6,15 +6,15 @@ import (
 
 type User struct {
 	ID        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
-	Username  string    `gorm:"type:varchar(50);not null" json:"username"`
-	Email     string    `gorm:"type:varchar(100);unique;not null" json:"email"`
-	Password  string    `gorm:"type:varchar(255);not null" json:"password"`
+	Username  string    `gorm:"type:varchar(50);not null" json:"username" form:"username"`
+	Email     string    `gorm:"type:varchar(100);unique;not null" json:"email" form:"email"`
+	Password  string    `gorm:"type:varchar(255);not null" json:"password" form:"password"`
 	Role      string    `gorm:"type:enum('admin', 'staff', 'user');default:'user';not null" json:"role"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
 
 type UserLogin struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required" form:"email"`
+	Password string `json:"password" binding:"required" form:"password"`
 }
