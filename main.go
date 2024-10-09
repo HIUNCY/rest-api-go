cf77b0fda980404d0b1209c919e0b3b27e6c013e
Muhamad Zainul Kamal <zainul.intern.pasti@pegadaian.co.id>
2024-10-09 08:43:24 +0700

    migrasi db to railway

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	db, err := gorm.Open(mysql.Open("sql12734405:HCwYzkUDCB@tcp(sql12.freesqldatabase.com:3306)/sql12734405?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:WyOuRPHnvSHOlRMmbRhzESwdWHCuPYVm@tcp(autorack.proxy.rlwy.net:56977)/tabungan?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
